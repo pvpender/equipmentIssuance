b52fa4a8ca097b5793ee41f5cd7cba96255bcdc2
Николай Моисеев <nicolaymoiseev1@gmail.com>
2022-11-19 20:59:59 +0300

    database

--- Work/Scripts/main.py
+++ Work/Scripts/main.py
@@ -7,21 +7,21 @@ from user_collections import *
 from database import *
 import pymysql
 
-engine = create_engine("mysql+pymysql://sql7544993:9razbAvzG4@sql7.freemysqlhosting.net/sql7544993")
-Base.metadata.create_all(engine)
-session = Session(engine)
-super = Admins(
-    mail="superuser",
-    access_id=0,
-    access=AdminAccesses(
-        can_insert=1,
-        can_delete=1,
-        can_change=1,
-        can_get_request=1
-    )
-)
-session.add(super)
-session.commit()
+#engine = create_engine("mysql+pymysql://sql7544993:9razbAvzG4@sql7.freemysqlhosting.net/sql7544993")
+#Base.metadata.create_all(engine)
+#session = Session(engine)
+#super = Admins(
+#    mail="superuser",
+#    access_id=0,
+#    access=AdminAccesses(
+#        can_insert=1,
+#        can_delete=1,
+#        can_change=1,
+#        can_get_request=1
+#    )
+#)
+#session.add(super)
+#session.commit()
 zero_admin = Admin(0, "superuser", "superpassword", AdminAccess(True, True, True, True, True))
 
 user_list = UserCollection()
