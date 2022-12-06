e9ccba4fe637d852c69e0857651336889af9ee34
Николай Моисеев <nicolaymoiseev1@gmail.com>
2022-12-06 23:23:14 +0300

    change accesses.py and equipment.py

--- Work/Scripts/accesses.py
+++ Work/Scripts/accesses.py
@@ -1,72 +1,58 @@
-from abc import ABC
-
 
 class Access:
-    def __init__(self, new_id: int, can_get_without_req: bool, can_send_req: bool):
-        self.__id = new_id
-        self.__can_get_without_req = can_get_without_req
-        self.__can_send_req = can_send_req
+    def __init__(self, power: int):
+        self.__power = power
 
     @property
-    def id(self) -> int:
-        return self.__id
+    def power(self) -> int:
+        return self.__power
 
-    @id.setter
-    def id(self, new_id: int):
-        self.__id = new_id
-
-    @property
-    def can_get_without_req(self) -> bool:
-        return self.__can_get_without_req
-
-    @can_get_without_req.setter
-    def can_get_without_req(self, new_list_eq: bool):
-        self.__can_get_without_req = new_list_eq
-
-    @property
-    def can_send_req(self) -> bool:
-        return self.__can_send_req
-
-    @can_send_req.setter
-    def can_send_req(self, new_opportunity: bool):
-        self.__can_send_req = new_opportunity
-# Переименовать can_get_without_request
+    @power.setter
+    def power(self, new_power: int):
+        self.__power = new_power
 
 
 class AdminAccess(Access):
-    def __init__(self, new_id: int, can_insert: bool, can_delete: bool, can_change: bool, can_get_request: bool,
-                 can_get_without_req:bool, power: int):
-        super().__init__(new_id, can_get_without_req, can_get_request)
-        self.__can_insert = can_insert
-        self.__can_delete = can_delete
-        self.__can_change = can_change
+    def __init__(self, power: int, can_add_users: bool, can_change_users: bool, can_add_inventory: bool,
+                 can_change_inventory: bool, can_get_request: bool):
+        super().__init__(power)
+        self.__can_add_users = can_add_users
+        self.__can_change_users = can_change_users
+        self.__can_add_inventory = can_add_inventory
+        self.__can_change_inventory = can_change_inventory
         self.__can_get_request = can_get_request
-        self.__can_get_without_req=can_get_without_req
-        self.__power=power ##Права относительно пользователей в виде числа. К примеру: 21, где 20 - властен добавлять, менять и удалять пользователей, 1 - властен добавлять админов
 
     @property
-    def can_insert(self) -> bool:
-        return self.__can_insert
+    def can_add_users(self) -> bool:
+        return self.__can_add_users
 
-    @can_insert.setter
-    def can_insert(self, new_opportunity: bool):
-        self.__can_insert = new_opportunity
-
-    @property
-    def can_delete(self) -> bool:
-        return self.__can_delete
-
-    @can_delete.setter
-    def can_delete(self, new_opportunity: bool):
-        self.__can_delete = new_opportunity
+    @can_add_users.setter
+    def can_add_users(self, new_opportunity: bool):
+        self.__can_add_users = new_opportunity
 
     @property
-    def can_change(self) -> bool:
-        return self.__can_change
+    def can_change_users(self) -> bool:
+        return self.__can_change_users
 
-    @can_change.setter
-    def can_change(self, new_opportunity: bool):
-        self.__can_change = new_opportunity
+    @can_change_users.setter
+    def can_change_users(self, new_opportunity: bool):
+        self.__can_change_users = new_opportunity
+
+    @property
+    def can_add_inventory(self) -> bool:
+        return self.__can_add_inventory
+
+    @can_add_inventory.setter
+    def can_add_inventory(self, new_opportunity: bool):
+        self.__can_add_inventory = new_opportunity
+
+    @property
+    def can_change_inventory(self) -> bool:
+        return self.__can_change_inventory
+
+    @can_change_inventory.setter
+    def can_change_inventory(self, new_opportunity: bool):
+        self.__can_change_inventory = new_opportunity
 
     @property
     def can_get_request(self) -> bool:
@@ -75,10 +61,3 @@ class AdminAccess(Access):
     @can_get_request.setter
     def can_get_request(self, new_opportunity: bool):
         self.__can_get_request = new_opportunity
-
-    @property
-    def power(self)->int:
-        return self.__power
-    @power.setter
-    def power(self, newPower: int):
-        self.__power=newPower
--- Work/Scripts/equipment.py
+++ Work/Scripts/equipment.py
@@ -2,12 +2,14 @@
 
 class Equipment:
 
-    def __init__(self, new_id: int, title: str, description: str, count: int, reserve_count: int):
+    def __init__(self, new_id: int, title: str, count: int, reserve_count: int, x: int, y: int):
         self.__id = new_id
         self.__title = title
-        self.__description = description
+        #self.__description = description
         self.__count = count
         self.__reserve_count = reserve_count
+        self.__x = x
+        self.__y = y
 
     @property
     def id(self) -> int:
@@ -25,13 +27,13 @@ class Equipment:
     def title(self, title: str):
         self.__title = title
 
-    @property
+    """@property
     def description(self) -> str:
         return self.__description
 
     @description.setter
     def description(self, new_description: str):
-        self.__description = new_description
+        self.__description = new_description"""
 
     @property
     def count(self) -> int:
@@ -48,3 +50,19 @@ class Equipment:
     @reserve_count.setter
     def reserve_count(self, new_res_count: int):
         self.__reserve_count = new_res_count
+
+    @property
+    def x(self) -> int:
+        return self.__x
+
+    @x.setter
+    def x(self, new_x: int):
+        self.__x = new_x
+
+    @property
+    def y(self) -> int:
+        return self.__y
+
+    @y.setter
+    def y(self, new_y: int):
+        self.__y = new_y
