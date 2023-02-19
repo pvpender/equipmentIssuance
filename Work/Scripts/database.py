dfffdea81581745e8fbc2612fb8003b36a5b8e87
pvpender <Nicolaymoiseev1@gmail.com>
2023-02-20 00:11:29 +0300

    change db

--- Work/Scripts/database.py
+++ Work/Scripts/database.py
@@ -103,9 +103,21 @@ def restart_if_except(function):
         except OperationalError:
             self.session.rollback()
             return function(*args, **kwargs)
+
     return check
 
 
+def for_all_methods(decorator):
+    def decorate(cls):
+        for attr in cls.__dict__:  # there's propably a better way to do this
+            if callable(getattr(cls, attr)):
+                setattr(cls, attr, decorator(getattr(cls, attr)))
+        return cls
+
+    return decorate
+
+
+@for_all_methods(restart_if_except)
 class DataBase:
 
     def __init__(self, session: Session):
@@ -114,6 +126,7 @@ class DataBase:
     @property
     def session(self):
         return self.__session
+
     """@staticmethod
     def __restart_if_except(function):
         def check(*args, **kwargs):
@@ -127,7 +140,6 @@ class DataBase:
         return check
         """
 
-    @restart_if_except
     def add_user(self, user: CommonUser):
         exist = self.__session.query(Users.id).filter(Users.mail == user.mail, Users.pass_number == user.id).first()
         if exist:
@@ -153,7 +165,6 @@ class DataBase:
         self.__session.add(db_user)
         self.__session.commit()
 
-    @restart_if_except
     def update_user(self, old_id: int, old_mail: str, user: CommonUser):
         access = user.access
         access_id = self.__session.query(Accesses.id).filter(
@@ -173,7 +184,6 @@ class DataBase:
         )
         self.__session.commit()
 
-    @restart_if_except
     def delete_user(self, user: CommonUser):
         try:
             self.__session.query(Users).filter_by(mail=user.mail, pass_number=user.id).delete()
@@ -182,22 +192,18 @@ class DataBase:
             self.__session.query(Users).filter_by(mail=user.mail, pass_number=user.id).delete()
         self.__session.commit()
 
-    @restart_if_except
     def get_user_by_id(self, pass_number: int):
         self.__session.commit()
         return self.__session.query(Users, Accesses).join(Accesses).filter(Users.pass_number == pass_number).first()
 
-    @restart_if_except
     def get_user_by_mail(self, mail: str):
         self.__session.commit()
         return self.__session.query(Users, Accesses).join(Accesses).filter(Users.mail == mail).first()
 
-    @restart_if_except
     def get_all_users(self):
         self.__session.commit()
         return self.__session.query(Users, Accesses).join(Accesses).all()
 
-    @restart_if_except
     def add_admin(self, admin: Admin):
         exist = self.__session.query(Admins.id).filter(Admins.mail == admin.mail,
                                                        Admins.pass_number == admin.id).first()
@@ -240,7 +246,6 @@ class DataBase:
         self.__session.add(db_admin)
         self.__session.commit()
 
-    @restart_if_except
     def update_admin(self, old_id: int, old_mail: str, admin: Admin):
         access = admin.access
         access_id = self.__session.query(AdminAccesses.id).filter(
@@ -276,28 +281,23 @@ class DataBase:
         )
         self.__session.commit()
 
-    @restart_if_except
     def delete_admin(self, admin: Admin):
         self.__session.query(Admins).filter(Admins.mail == admin.mail, Admins.pass_number == admin.id).delete()
         self.__session.commit()
 
-    @restart_if_except
     def get_admin_by_id(self, pass_number: int):
         self.__session.commit()
         return self.__session.query(Admins, AdminAccesses).join(AdminAccesses).filter(
             Admins.pass_number == pass_number).first()
 
-    @restart_if_except
     def get_admin_by_mail(self, mail: str):
         self.__session.commit()
         return self.__session.query(Admins, AdminAccesses).join(AdminAccesses).filter(Admins.mail == mail).first()
 
-    @restart_if_except
     def get_all_admins(self):
         self.__session.commit()
         return self.__session.query(Admins, AdminAccesses).join(AdminAccesses).all()
 
-    @restart_if_except
     def add_equipment(self, equipment: Equipment):
         exist = self.__session.query(Equipments.id).filter(Equipments.title == equipment.title).first()
         if exist:
@@ -315,7 +315,6 @@ class DataBase:
         self.__session.commit()
         equipment.id = self.get_equipment_by_title(equipment.title).id
 
-    @restart_if_except
     def update_equipment(self, old_id: int, equipment: Equipment):
         if not (equipment.x and equipment.y):
             exist = self.__session.query(Equipments).filter(
@@ -329,22 +328,18 @@ class DataBase:
              "access": equipment.access, "x": equipment.x, "y": equipment.y}, synchronize_session=False
         )
 
-    @restart_if_except
     def delete_equipment(self, equipment: Equipment):
         self.__session.query(Equipments).filter(Equipments.title == equipment.title).delete()
         self.__session.commit()
 
-    @restart_if_except
     def get_equipment_by_title(self, title: str):
         self.__session.commit()
         return self.__session.query(Equipments).filter(Equipments.title == title).first()
 
-    @restart_if_except
     def get_all_equipment(self):
         self.__session.commit()
         return self.__session.query(Equipments).all()
 
-    @restart_if_except
     def get_equipment_by_coordinates(self, x: int, y: int):
         self.__session.commit()
         if x != -1 and y != -1:
@@ -352,22 +347,14 @@ class DataBase:
         else:
             return None
 
-    @restart_if_except
     def get_equipment_by_id(self, eq_id: int):
         self.__session.commit()
         return self.__session.query(Equipments).filter(Equipments.id == eq_id).first()
 
-    @restart_if_except
-    def get_all_equipment(self):
-        self.__session.commit()
-        return self.__session.query(Equipments).all()
-
-    @restart_if_except
     def get_tg_user(self, user_id):
         self.__session.commit()
         return self.__session.query(TelegramLogins).filter(TelegramLogins.user_id == user_id).first()
 
-    @restart_if_except
     def add_tg_user(self, user_id, mail, power):
         db_user = TelegramLogins(
             user_id=user_id,
@@ -382,7 +369,6 @@ class DataBase:
             )
         self.__session.commit()
 
-    @restart_if_except
     def add_request(self, request: req.Request):
         db_request = UserRequests(
             sender_tg_id=request.sender_tg_id,
@@ -399,29 +385,24 @@ class DataBase:
         self.__session.add(db_request)
         self.__session.commit()
 
-    @restart_if_except
     def get_all_requests(self):
         self.__session.commit()
         return self.__session.query(UserRequests).all()
 
-    @restart_if_except
     def get_solved_requests(self):
         self.__session.commit()
         return self.__session.query(UserRequests).filter(UserRequests.solved is True).all()
 
-    @restart_if_except
     def get_unsolved_requests(self):
         self.__session.commit()
         return self.__session.query(UserRequests).filter((UserRequests.solved is False) or
                                                          (UserRequests.solved.is_(None))).all()
 
-    @restart_if_except
     def get_first_unsolved_request(self):
         self.__session.commit()
         return self.__session.query(UserRequests).filter((UserRequests.solved is False) or
                                                          (UserRequests.solved.is_(None))).first()
 
-    @restart_if_except
     def add_last_request(self, user_id: int, title: str, description: str):
         self.__session.query(LastRequest).filter(LastRequest.user_id == user_id).delete()
         last_request = LastRequest(
@@ -431,12 +412,10 @@ class DataBase:
         self.__session.add(last_request)
         self.__session.commit()
 
-    @restart_if_except
     def get_last_request(self, user_id):
         self.__session.commit()
         return self.__session.query(LastRequest).filter(LastRequest.user_id == user_id).first()
 
-    @restart_if_except
     def update_request(self, request: UserRequests):
         self.__session.query(UserRequests).filter(UserRequests.sender_tg_id == request.sender_tg_id,
                                                   UserRequests.title == request.title,
