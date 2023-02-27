9e1fa5a79188e1669d55091873aa54eab9d0aa24
pvpender <Nicolaymoiseev1@gmail.com>
2023-02-27 22:44:19 +0300

    fixes changes

--- Work/Scripts/main.py
+++ Work/Scripts/main.py
@@ -13,8 +13,8 @@ import pymysql
 
 import datetime
 
-#engine = create_engine("mysql+pymysql://freedb_testadminuser:#q4UD$mVTfVrscM@sql.freedb.tech/freedb_Testbase")
-engine = create_engine("mysql+pymysql://admin:testPass@194.67.206.233:3306/test_base")
+engine = create_engine("mysql+pymysql://freedb_testadminuser:#q4UD$mVTfVrscM@sql.freedb.tech/freedb_Testbase")
+
 Base.metadata.create_all(engine)
 sqlalchemy.pool_recycle = 10
 sqlalchemy.pool_timeout = 20
