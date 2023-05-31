95c6f82e7a27ffe10c44d14e9fa9ea3a332cfcbc
pvpender <Nicolaymoiseev1@gmail.com>
2023-05-31 19:48:20 +0300

    фикс к защите

--- Work/Scripts/rubbish_collector.py
+++ Work/Scripts/rubbish_collector.py
@@ -3,12 +3,13 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 import time
 import threading
+from conf import BASE_URL
 
 
 def clear_connection(db: DataBase):
     if (time.time() - db.last_db_access_time) > 200:
         t = threading.Thread(target=fix_died_connection, args=(db.session,))
         t.start()
-        db.session = Session(create_engine(f"mysql+pymysql://admin:testPass@194.67.206.233:3306/test_base"))
+        db.session = Session(create_engine(BASE_URL))
         db.last_db_access_time = time.time()
 
